8744f3a8fee83f586fd01b5fc5ad07e45da16f96
MJefferson <marq.d.jefferson@gmail.com>
2012-06-13 20:57:31 -0400

    adds history

--- public/javascripts/views.js
+++ public/javascripts/views.js
@@ -1,5 +1,6 @@
 
 meta = {};
+history = new Array();
 lens = document.location.href.substr(document.location.href.lastIndexOf('/'));
 
 $(document).ready(function(){
@@ -7,14 +8,57 @@ $(document).ready(function(){
 	meta.uid = $('#instance').html();
 	refreshSaves();
 	window.setInterval(refreshSaves, 1000);
+	function addToHistory(obj){
+		if(history.length <= 50 && history.indexOf(obj) == -1){
+			history.push(obj);
+		} else {
+			history.shift();
+			history.push(obj);
+		}
+	}
+	function previousEntry(){
+		
+		if(history.length < 1){
+			return false;
+		}
+		
+		var position = history.indexOf(meta);
+		
+		if(history.length > 0 && position > 0 ){
+			var previous = history[position-1];
+			meta = previous;
+			$('h1.phrase').html(previous.phrase);
+		} else if (position == -1 ){
+			var previous = history[history.length-1];
+			addToHistory(meta);
+			meta = previous;
+			$('h1.phrase').html(previous.phrase);
+		} else if (position == 0) {
+			return false;
+		} else {
+			return false;
+		}
+		
+	}
 	function nextEntry(){
-		$.ajax({
-			url: lens + '.json',
-			success: function(data){
-				$('h1.phrase').html(data.phrase);
-				meta = data;
-			}
-		});
+		var position = history.indexOf(meta);
+		
+		if (position > -1 && history.length > (position + 1)) {
+			//load next history
+			var next = history[position+1];
+			$('h1.phrase').html(next.phrase);
+			meta = next;
+			
+		} else {
+			$.ajax({
+				url: lens + '.json',
+				success: function(data){
+					addToHistory(data);
+					$('h1.phrase').html(data.phrase);
+					meta = data;
+				}
+			});
+		}
 	}
 	function refreshSaves(){
 		$.ajax({
@@ -34,6 +78,9 @@ $(document).ready(function(){
 		}
 		if(event.which == 38){
 			refreshSaves();
+		}
+		if(event.which == 37){
+			previousEntry();
 		}
 	});
 	$('#next').bind('click', function(){
